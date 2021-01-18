87d6944a99545155952bd089d9ca53000644a7e3
Gemma Dingwall <gemma9261@hotmail.com>
2021-01-18 15:29:29 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,16 @@
+# Defining my string variable
+
+string = "gemma loves PIZZA"
+puts "The string I will use to show text manipulation will be...[#{string}] "
+
+string1 = string.upcase!
+puts string1
+
+string2 = string.capitalize!
+puts string2
+
+string3 = string.swapcase!
+puts string3
+
+string4 = string.reverse!
+puts string4
