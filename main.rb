6813e880541230d5ce69705d5c6f8ee51d50cb32
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-22 13:19:55 +0000

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,15 +2,15 @@
 
 string = "gemma loves PIZZA"
 puts "The string I will use to show text manipulation will be...[#{string}] "
-
+# Converting the string to uppercase
 string1 = string.upcase!
 puts string1
-
+# Capitalising the first letter of the string
 string2 = string.capitalize!
 puts string2
-
+# Swapping the case of any given character to the opposite of its original
 string3 = string.swapcase!
 puts string3
-
+# Reversing the string
 string4 = string.reverse!
 puts string4
